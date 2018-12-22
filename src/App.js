1448d21812ca385bf585c33af6aab1cc48b64e09
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-12-22 19:53:09 +0800

    completed build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { DateRangePicker} from 'react-dates';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.filter = {
+
+    }
+
+    this.state = {
+      focusedInput: 'startDate',
+      startDate: moment(), // set your initial start date here
+      endDate: moment().add(3, 'days'), // set your initial end date here
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>DatePicker</h1>
+        <DateRangePicker
+          startDate={this.state.startDate} 
+          startDateId="start-date" 
+          endDate={this.state.endDate}
+          endDateId="end-date"
+          onDatesChange={({ startDate, endDate }) => {
+            this.setState({ startDate, endDate })
+            }
+          }
+          onClose = {({startDate, endDate}) => {
+          }}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={focusedInput => this.setState({ focusedInput })}
+          orientation="vertical"
+          withFullScreenPortal
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import jQuery from 'jquery';
 import * as serviceWorker from './serviceWorker';
+
+
+// could use fallback option for older mobile browsers
+const urlParams = new URLSearchParams(window.location.search);
+const userId = urlParams.get('userId');
+const platform = urlParams.get('platform');
+
+const submitButton = document.getElementById('submit');
+
+let dates = {};
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+console.log(document.getElementById('start-date'));
+
 serviceWorker.unregister();
+
+submitButton.addEventListener('click', (e) => {
+  const startDate = document.getElementById('start-date').value;
+  const endDate = document.getElementById('end-date').value;
+  let url = 'https://hotel-dev-225712.firebaseio.com/users.json'
+  let options = {
+    method: 'POST',
+    body: {
+      user: userId,
+      platform: platform,
+      variables: {
+        checkin: startDate,
+        checkout: endDate,
+      },
+      next_step: 'onboard_complete'
+    }
+  }
+  console.log(options)
+ 
+})
+
+
